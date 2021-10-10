1b6f5af8bbbacbad290751e4ff7ab9d67ee93f01
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-10-10 14:35:18 -0300

    Configure build environment.

---
+++ dragon_blaster.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lib/SMSlib.h"
+#include "lib/PSGlib.h"
+#include "data.h"
+
+void load_standard_palettes() {
+	SMS_loadBGPalette(sprites_palette_bin);
+	SMS_loadSpritePalette(sprites_palette_bin);
+	SMS_setSpritePaletteColor(0, 0);
+}
+
+void main() {
+	SMS_useFirstHalfTilesforSprites(1);
+	SMS_setSpriteMode(SPRITEMODE_TALL);
+	SMS_VDPturnOnFeature(VDPFEATURE_HIDEFIRSTCOL);
+
+	SMS_displayOff();
+	SMS_loadPSGaidencompressedTiles(sprites_tiles_psgcompr, 0);
+	load_standard_palettes();
+
+	SMS_displayOn();
+	
+
+	while (1) {	
+		SMS_initSprites();	
+		SMS_finalizeSprites();
+		SMS_waitForVBlank();
+		SMS_copySpritestoSAT();
+	}
+}
+
+SMS_EMBED_SEGA_ROM_HEADER(9999,0); // code 9999 hopefully free, here this means 'homebrew'
+SMS_EMBED_SDSC_HEADER(0,1, 2021,10,10, "Haroldo-OK\\2021", "Dragon Blaster",
+  "A dragon-themed shoot-em-up.\n"
+  "Made for the SHMUP JAM 1 - Dragons - https://itch.io/jam/shmup-jam-1-dragons\n"
+  "Built using devkitSMS & SMSlib - https://github.com/sverx/devkitSMS");
