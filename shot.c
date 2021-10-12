c4253d12110fab4d8c9b85973cf374413d001127
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-10-12 09:08:57 -0300

    Add "hellfire" shot.

--- shot.c
+++ shot.c
@@ -29,6 +29,30 @@ const path_step fire_path[] = {
 	{-128, -128}
 };
 
+const path_step hellfire_path[] = {
+	{0, -2},
+	{5, -2},
+	{5, -2},
+	{4, -2},
+	{3, -2},
+	{0, -2},
+	{-3, -2},
+	{-4, -2},
+	{-5, -2},
+	{-5, -2},
+	{-5, -2},
+	{-5, -2},
+	{-4, -2},
+	{-3, -2},
+	{0, -2},
+	{3, -2},
+	{4, -2},
+	{5, -2},
+	{5, -2},
+	{0, -2},
+	{-128, -128}
+};
+
 const path_step wind_path0[] = {
 	{-2, -3},
 	{-128, -128}
@@ -52,6 +76,10 @@ const path fire_paths[] = {
 	{8, -8, 0, fire_path}
 };
 
+const path hellfire_paths[] = {
+	{8, -8, 0, hellfire_path}
+};
+
 const path wind_paths[] = {
 	{6, -6, 0, wind_path0},
 	{8, -8, 0, wind_path1},
@@ -60,7 +88,8 @@ const path wind_paths[] = {
 
 const shot_info player_shot_infos[PLAYER_SHOT_TYPE_COUNT] = {
 	{26, 3, 45, 4, 1, lightining_paths},
-	{32, 4, 45, 8, 1, fire_paths},
+	{32, 4, 40, 8, 1, fire_paths},
 	{40, 2, 45, 12, 3, wind_paths},
 	{44, 3, 45, 4, 1, lightining_paths},
+	{32, 4, 55, 8, 1, hellfire_paths},
 };
