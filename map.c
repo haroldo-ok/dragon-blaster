3aca1e7c2a4e40abe52dd0fc7983f70ca2f21ff9
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-11-04 19:13:16 -0300

    Implement map decompression.

--- map.c
+++ map.c
@@ -22,22 +22,56 @@ void init_map(void *level_data) {
 	map_data.scroll_y = 0;
 }
 
+void decompress_map_row(char *buffer) {
+	static char *o, *d;
+	static char remaining, ch, repeat;
+	
+	o = map_data.next_row;
+	d = buffer;
+	for (remaining = 16; remaining; ) {
+		ch = *o;
+		o++;
+		
+		if (ch & 0x80) {
+			// Has repeat flag: repeat n times
+			repeat = ch & 0x7F;
+			ch = *o;
+			o++;
+			
+			for (; repeat && remaining; repeat--, remaining--) {
+				*d = ch;
+				d++;
+			}
+		} else {
+			// Just use the char
+			*d = ch;
+			d++;
+			remaining--;
+		}
+	}
+	
+	map_data.next_row = o;
+}
+
 void draw_map_row() {
 	static char i, j;
 	static char y;
 	static char *map_char;
 	static unsigned int base_tile, tile;
+	static char buffer[16];
+
+	decompress_map_row(buffer);
 
 	for (i = 2, y = map_data.background_y, base_tile = 256; i; i--, y++, base_tile++) {
 		SMS_setNextTileatXY(0, y);
-		for (j = 16, map_char = map_data.next_row; j; j--, map_char++) {
+		for (j = 16, map_char = buffer; j; j--, map_char++) {
 			tile = base_tile + (*map_char << 2);
 			SMS_setTile(tile);
 			SMS_setTile(tile + 2);
 		}
 	}
 	
-	map_data.next_row += 16;
+	//map_data.next_row += 16;
 	if (*map_data.next_row == 0xFF) {
 		// Reached the end of the map; reset
 		map_data.next_row = map_data.level_data;
