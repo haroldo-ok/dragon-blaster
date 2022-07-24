209b7b9bcef620ac73ff282fa839de4240a8a8a7
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-07-24 16:16:45 -0300

    Add one object to the map.

---
+++ data/map/sprites.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.03.23" name="sprites" tilewidth="16" tileheight="16" tilecount="64" columns="16">
+ <image source="../img/sprites.png" width="256" height="64"/>
+</tileset>
