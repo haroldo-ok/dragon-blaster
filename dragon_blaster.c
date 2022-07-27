269a3e86e0d56b097fd3e8f52e79ef9916328400
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2022-07-27 18:53:07 -0300

    Bump version to 0.11

--- dragon_blaster.c
+++ dragon_blaster.c
@@ -719,7 +719,7 @@ void main() {
 }
 
 SMS_EMBED_SEGA_ROM_HEADER(9999,0); // code 9999 hopefully free, here this means 'homebrew'
-SMS_EMBED_SDSC_HEADER(0,10, 2022,07,27, "Haroldo-OK\\2022", "Dragon Blaster",
+SMS_EMBED_SDSC_HEADER(0,11, 2022,07,27, "Haroldo-OK\\2022", "Dragon Blaster",
   "A dragon-themed shoot-em-up.\n"
   "Originally made for the SHMUP JAM 1 - Dragons - https://itch.io/jam/shmup-jam-1-dragons\n"
   "Enhanced for SMS Power! Competition 2022 - https://www.smspower.org/forums/18879-Competitions2022DeadlineIs27thMarch\n"
